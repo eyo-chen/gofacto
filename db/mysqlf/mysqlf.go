39eb56a8588b30235a44b976c61c0143f624524b
CHEN YI YOU <zxcvbn1239104@gmail.com>
2024-08-03 11:18:43 +0800

    Refactor/add sqllib to extract sql logic (#14)
    * feat: add sqllib
    
    * refactor: use sqllib
    
    * fix: handle error
    
    * refactor: use db interface
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

--- db/mysqlf/mysqlf.go
+++ db/mysqlf/mysqlf.go
@@ -3,172 +3,29 @@ package mysqlf
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
-	"reflect"
-	"strings"
 
 	"github.com/eyo-chen/gofacto/db"
-	"github.com/eyo-chen/gofacto/internal/utils"
+	"github.com/eyo-chen/gofacto/internal/sqllib"
 )
 
-// config is for MySQL database configuration
-type config struct {
-	// db is the database connection
-	db *sql.DB
+// NewConfig initializes interface for raw mySQL database operations
+func NewConfig(db *sql.DB) db.Database {
+	return sqllib.NewConfig(db, &mySQLDialect{}, "mysqlf")
 }
 
-// NewConfig creates a new MySQL configuration
-func NewConfig(db *sql.DB) *config {
-	return &config{
-		db: db,
-	}
+// mySQLDialect defines the behavior for MySQL SQL dialect
+type mySQLDialect struct{}
+
+func (d *mySQLDialect) GenPlaceholder(_ int) string {
+	return "?"
 }
 
-func (c *config) Insert(ctx context.Context, params db.InserParams) (interface{}, error) {
-	rawStmt, vals := prepareStmtAndVals(params.StorageName, params.Value)
-
-	stmt, err := c.db.Prepare(rawStmt)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-
-	tx, err := c.db.Begin()
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if rollbackErr := tx.Rollback(); rollbackErr != nil && !errors.Is(rollbackErr, sql.ErrTxDone) && err == nil {
-			err = rollbackErr
-		}
-	}()
-
-	id, err := insertToDB(ctx, tx, stmt, vals[0])
-	if err != nil {
-		return nil, err
-	}
-
-	setIDField(params.Value, id)
-	if err := tx.Commit(); err != nil {
-		return nil, err
-	}
-
-	return params.Value, nil
+func (d *mySQLDialect) GenInsertStmt(tableName, fieldNames, placeholder string) string {
+	return fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, fieldNames, placeholder)
 }
 
-func (c *config) InsertList(ctx context.Context, params db.InserListParams) ([]interface{}, error) {
-	rawStmt, fieldValues := prepareStmtAndVals(params.StorageName, params.Values...)
-
-	stmt, err := c.db.Prepare(rawStmt)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-
-	tx, err := c.db.Begin()
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if rollbackErr := tx.Rollback(); rollbackErr != nil && !errors.Is(rollbackErr, sql.ErrTxDone) && err == nil {
-			err = rollbackErr
-		}
-	}()
-
-	result := make([]interface{}, len(fieldValues))
-	for i, vals := range fieldValues {
-		id, err := insertToDB(ctx, tx, stmt, vals)
-		if err != nil {
-			return nil, err
-		}
-
-		v := params.Values[i]
-		setIDField(v, id)
-
-		result[i] = v
-	}
-
-	if err := tx.Commit(); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func (c *config) SetIDField(v interface{}, i int) error {
-	if reflect.ValueOf(v).Kind() != reflect.Ptr {
-		return fmt.Errorf("SetIDField: argument must be a pointer")
-	}
-
-	if reflect.ValueOf(v).Elem().Kind() != reflect.Struct {
-		return fmt.Errorf("SetIDField: argument must be a pointer to a struct")
-	}
-
-	val := reflect.ValueOf(v).Elem()
-	idField := val.FieldByName("ID")
-
-	if !idField.IsValid() {
-		return fmt.Errorf("SetIDField: ID field not found")
-	}
-
-	if !idField.CanSet() {
-		return fmt.Errorf("SetIDField: ID field is not settable")
-	}
-
-	switch idField.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		idField.SetInt(int64(i))
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		idField.SetUint(uint64(i))
-	}
-
-	return nil
-}
-
-// prepareStmtAndVals prepares the SQL insert statement and the values to be inserted
-// values are the pointer to the struct
-func prepareStmtAndVals(tableName string, values ...interface{}) (string, [][]interface{}) {
-	fieldNames := []string{}
-	placeholders := []string{}
-	fieldValues := [][]interface{}{}
-
-	for index, val := range values {
-		val := reflect.ValueOf(val).Elem()
-		vals := []interface{}{}
-
-		for i := 0; i < val.NumField(); i++ {
-			n := val.Type().Field(i).Name
-			if n == "ID" {
-				continue
-			}
-
-			vals = append(vals, val.Field(i).Interface())
-
-			if index == 0 {
-				fieldName := val.Type().Field(i).Tag.Get("sqlf")
-				if fieldName == "" {
-					fieldName = utils.CamelToSnake(n)
-				}
-
-				fieldNames = append(fieldNames, fieldName)
-				placeholders = append(placeholders, "?")
-			}
-		}
-
-		fieldValues = append(fieldValues, vals)
-	}
-
-	// construct the SQL insert statement
-	fns := strings.Join(fieldNames, ", ")
-	phs := strings.Join(placeholders, ", ")
-	rawStmt := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, fns, phs)
-
-	return rawStmt, fieldValues
-}
-
-// insertToDB inserts the given values to the database
-func insertToDB(ctx context.Context, tx *sql.Tx, stmt *sql.Stmt, vals []interface{}) (int64, error) {
+func (d *mySQLDialect) InsertToDB(ctx context.Context, tx *sql.Tx, stmt *sql.Stmt, vals []interface{}) (int64, error) {
 	res, err := tx.Stmt(stmt).ExecContext(ctx, vals...)
 	if err != nil {
 		return 0, err
@@ -180,16 +37,4 @@ func insertToDB(ctx context.Context, tx *sql.Tx, stmt *sql.Stmt, vals []interfac
 	}
 
 	return id, nil
-}
-
-// setIDField sets the id value on ID field of the given value
-func setIDField(v interface{}, id int64) {
-	val := reflect.ValueOf(v).Elem()
-	idField := val.FieldByName("ID")
-	switch idField.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		idField.SetInt(id)
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		idField.SetUint(uint64(id))
-	}
 }
---
+++ internal/sqllib/sqllib.go
@@ -0,0 +1,176 @@
+package sqllib
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"reflect"
+	"strings"
+
+	"github.com/eyo-chen/gofacto/db"
+	"github.com/eyo-chen/gofacto/internal/utils"
+)
+
+// Config is for raw SQL database operations
+type Config struct {
+	// db is the database connection
+	db *sql.DB
+
+	// dialect is the SQL dialect
+	dialect sqlDialect
+
+	// packageName is the package name
+	packageName string
+}
+
+// sqlDialect defines the behavior for different SQL dialects
+type sqlDialect interface {
+	// GenPlaceholder generates a placeholder
+	GenPlaceholder(placeholderIdx int) string
+
+	// GenInsertStmt generates an insert raw SQL statement
+	GenInsertStmt(tableName, fieldNames, placeholder string) string
+
+	// InsertToDB inserts the values to the database
+	InsertToDB(ctx context.Context, tx *sql.Tx, stmt *sql.Stmt, vals []interface{}) (int64, error)
+}
+
+// NewConfig initializes a sqllib config for raw SQL database operations
+func NewConfig(db *sql.DB, dialect sqlDialect, packageName string) db.Database {
+	return &Config{
+		db:          db,
+		dialect:     dialect,
+		packageName: packageName,
+	}
+}
+
+func (c *Config) Insert(ctx context.Context, params db.InserParams) (interface{}, error) {
+	rawStmt, vals := c.prepareStmtAndVals(params.StorageName, params.Value)
+
+	stmt, err := c.db.Prepare(rawStmt)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	tx, err := c.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil && !errors.Is(rollbackErr, sql.ErrTxDone) && err == nil {
+			err = rollbackErr
+		}
+	}()
+
+	id, err := c.dialect.InsertToDB(ctx, tx, stmt, vals[0])
+	if err != nil {
+		return nil, err
+	}
+
+	setIDField(params.Value, id)
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return params.Value, nil
+}
+
+func (c *Config) InsertList(ctx context.Context, params db.InserListParams) ([]interface{}, error) {
+	rawStmt, fieldValues := c.prepareStmtAndVals(params.StorageName, params.Values...)
+
+	stmt, err := c.db.Prepare(rawStmt)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	tx, err := c.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil && !errors.Is(rollbackErr, sql.ErrTxDone) && err == nil {
+			err = rollbackErr
+		}
+	}()
+
+	result := make([]interface{}, len(fieldValues))
+	for i, vals := range fieldValues {
+		id, err := c.dialect.InsertToDB(ctx, tx, stmt, vals)
+		if err != nil {
+			return nil, err
+		}
+
+		v := params.Values[i]
+		setIDField(v, id)
+
+		result[i] = v
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// prepareStmtAndVals prepares the SQL insert statement and the values to be inserted
+// values are the pointer to the struct
+func (c *Config) prepareStmtAndVals(tableName string, values ...interface{}) (string, [][]interface{}) {
+	fieldNames := []string{}
+	placeholders := []string{}
+	fieldValues := [][]interface{}{}
+
+	for index, val := range values {
+		val := reflect.ValueOf(val).Elem()
+		vals := []interface{}{}
+
+		placeholderIndex := 1
+		for i := 0; i < val.NumField(); i++ {
+			n := val.Type().Field(i).Name
+			if n == "ID" {
+				continue
+			}
+
+			vals = append(vals, val.Field(i).Interface())
+
+			if index == 0 {
+				fieldName := val.Type().Field(i).Tag.Get(c.packageName)
+				if fieldName == "" {
+					fieldName = utils.CamelToSnake(n)
+				}
+
+				fieldNames = append(fieldNames, fieldName)
+				placeholders = append(placeholders, c.dialect.GenPlaceholder(placeholderIndex))
+			}
+
+			placeholderIndex++
+		}
+
+		fieldValues = append(fieldValues, vals)
+	}
+
+	// construct the SQL insert statement
+	fns := strings.Join(fieldNames, ", ")
+	phs := strings.Join(placeholders, ", ")
+	rawStmt := c.dialect.GenInsertStmt(tableName, fns, phs)
+
+	return rawStmt, fieldValues
+}
+
+// setIDField sets the id value on ID field of the given value
+func setIDField(v interface{}, id int64) {
+	val := reflect.ValueOf(v).Elem()
+	idField := val.FieldByName("ID")
+	if !idField.IsValid() || !idField.CanSet() {
+		return
+	}
+
+	switch idField.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		idField.SetInt(id)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		idField.SetUint(uint64(id))
+	}
+}
